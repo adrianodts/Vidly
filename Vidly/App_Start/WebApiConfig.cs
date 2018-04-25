c25734f115a5d8776bc40fb152df8935f3be01fa
Adriano Dantas <adrianodts@msn.com>
2018-04-25 03:46:46 -0300

    Adjust JsonFormatter settings

--- Vidly/App_Start/WebApiConfig.cs
+++ Vidly/App_Start/WebApiConfig.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Vidly
 {
@@ -10,6 +12,10 @@ namespace Vidly
     {
         public static void Register(HttpConfiguration config)
         {
+            var formatters = config.Formatters.JsonFormatter.SerializerSettings;
+            formatters.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            formatters.Formatting = Formatting.Indented;
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
